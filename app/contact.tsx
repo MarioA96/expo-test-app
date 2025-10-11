07aa15f94ef3469ace2e5805aba980269653b0cd
QUIROZ SOLDAN MARIO ALBERTO <marioaquirozs@gmail.com>
2025-10-10 19:39:22 -0600

    feat: replace default components with themed components in Contact screen

--- app/contact.tsx
+++ app/contact.tsx
@@ -1,13 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, useColorScheme } from 'react-native'
 import { Link } from 'expo-router'
 
-const Contact = () => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Contact</Text>
 
-      <Link href="/" style={styles.link}>Back Home</Link>
-    </View>
+// Themed components
+import ThemedView from '../components/ThemedView';
+import ThemedText from '../components/ThemedText';
+
+const Contact = () => {
+
+
+  return (
+    <ThemedView style={styles.container}>
+      <ThemedText style={styles.title}>Contact Page</ThemedText>
+
+      <Link href="/" style={styles.link}>
+        <ThemedText>Back Home</ThemedText>
+      </Link>
+    </ThemedView>
   )
 }
 
