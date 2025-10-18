78a6d29d7ddf6587b8f0af7d75dc8d3601602727
QUIROZ SOLDAN MARIO ALBERTO <marioaquirozs@gmail.com>
2025-10-17 20:35:57 -0600

    feat: add Profile component with themed styling and layout

---
+++ app/(dashboard)/profile.tsx
@@ -0,0 +1,38 @@
+import { StyleSheet } from 'react-native';
+
+import Spacer from '../../components/Spacer';
+import ThemedText from '../../components/ThemedText';
+import ThemedView from '../../components/ThemedView';
+
+const Profile = () => {
+
+    return (
+        <ThemedView style={styles.container}>
+
+            <ThemedText title={true} style={styles.heading}>
+                Your email
+            </ThemedText>
+            <Spacer />
+
+            <ThemedText>Time to start reading some books...</ThemedText>
+            <Spacer />
+
+        </ThemedView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    heading: {
+        fontWeight: 'bold',
+        fontSize: 18,
+        textAlign: 'center'
+    }
+})
+
+export default Profile;
