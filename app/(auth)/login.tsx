019822c4294733f16d17c24ba2c13a3c60ff4741
QUIROZ SOLDAN MARIO ALBERTO <marioaquirozs@gmail.com>
2025-10-17 20:35:17 -0600

    feat: enhance Login component with ThemedButton and improved styling

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,19 +1,30 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Pressable, Text } from 'react-native';
 import { Link } from 'expo-router';
+import { Colors } from '../../constants/Colors';
 
 // themed components
 import ThemedView from '../../components/ThemedView';
 import ThemedText from '../../components/ThemedText';
 import Spacer from '../../components/Spacer';
+import ThemedButton from '../../components/ThemedButton';
 
 const Login = () => {
+
+    const handleSubmit = () => {
+        console.log("Login for submit");
+    }
+
     return (
         <ThemedView style={styles.container}>
-            
+
             <Spacer />
             <ThemedText title={true} style={styles.title}>
                 Login to your account
             </ThemedText>
+
+            <ThemedButton onPress={handleSubmit}>
+                <Text style={{color: '#f2f2f2'}}>Login</Text>
+            </ThemedButton>
 
             <Spacer height={100}/>
 
@@ -32,7 +43,8 @@ export default Login;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: "center"
+        justifyContent: "center",
+        alignItems: 'center'
     },
 
     title: {
@@ -40,5 +52,15 @@ const styles = StyleSheet.create({
         fontSize: 18,
         fontWeight: "bold",
         marginBottom: 30
+    },
+
+    btn : {
+        backgroundColor: Colors.primary,
+        padding: 15,
+        borderRadius: 5,
+    },
+
+    pressed: {
+        opacity: 0.8
     }
 })
