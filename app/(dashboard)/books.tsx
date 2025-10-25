e80d4f3335c5052cf17dbf90cb41ed1f8801e568
QUIROZ SOLDAN MARIO ALBERTO <marioaquirozs@gmail.com>
2025-10-24 21:34:29 -0600

    feat: add Books component with layout and themed styling

---
+++ app/(dashboard)/books.tsx
@@ -0,0 +1,35 @@
+import { StyleSheet } from 'react-native';
+
+import Spacer from '../../components/Spacer';
+import ThemedText from '../../components/ThemedText';
+import ThemedView from '../../components/ThemedView';
+
+const Books = () => {
+
+    return (
+        <ThemedView style={styles.container} safe={true}>
+
+            <Spacer />
+            <ThemedText title={true} style={styles.heading}>
+                Your reading list
+            </ThemedText>
+
+        </ThemedView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        // justifyContent: 'center',
+        alignItems: 'stretch'
+    },
+
+    heading: {
+        fontWeight: 'bold',
+        fontSize: 18,
+        textAlign: 'center'
+    }
+})
+
+export default Books;
