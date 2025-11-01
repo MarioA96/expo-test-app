2bb714356861c1ea899744021e28d502c4d2d683
QUIROZ SOLDAN MARIO ALBERTO <marioaquirozs@gmail.com>
2025-10-31 21:49:24 -0600

    feat: enhance ThemedView component with safe area support

--- components/ThemedView.tsx
+++ components/ThemedView.tsx
@@ -2,19 +2,38 @@ import { ReactNode } from 'react';
 
 import { View, useColorScheme } from 'react-native';
 import {Colors} from '../constants/Colors'
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 interface Props {
     style?: object;
+    safe?: boolean;
     children?: ReactNode;
 }
 
-const ThemedView = ({style, ...props}: Props) => {
+const ThemedView = ({style, safe=false, ...props}: Props) => {
     const colorScheme = useColorScheme();
     const theme = Colors[colorScheme!] ?? Colors.light;
     
+    if(!safe) return (
+        <View
+            style={[ {backgroundColor: theme.background},style ]}
+            {...props}
+        />
+    )
+
+    const insets = useSafeAreaInsets();
+
     return (
-        <View style={[ {backgroundColor: theme.background},style ]}
-        {...props}
+        <View
+            style={[
+                {
+                    backgroundColor: theme.background,
+                    paddingTop: insets.top,
+                    paddingBottom: insets.bottom
+                },
+                style
+            ]}
+            {...props}
         />
     )
 }
