ce4029ca1d4aee5c2867995339527e3fda59ea7c
QUIROZ SOLDAN MARIO ALBERTO <marioaquirozs@gmail.com>
2025-10-31 21:49:56 -0600

    feat: wrap RootLayout in UserProvider for context support

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,16 @@
-import { Slot, Stack } from 'expo-router';
-import { Text, StyleSheet, View, useColorScheme } from 'react-native';
+import { Stack } from 'expo-router';
+import { useColorScheme } from 'react-native';
 import { Colors } from '../constants/Colors'
 import { StatusBar } from 'expo-status-bar';
+import { UserProvider } from '../contexts/UserContext';
 
 const RootLayout = () => {
-    
+
     const colorScheme = useColorScheme();
     const theme = Colors[colorScheme!] ?? Colors.light;
 
     return (
-        <>
+        <UserProvider>
             <StatusBar style="auto" />
             <Stack screenOptions={{
                 headerStyle: { backgroundColor: theme.navBackground },
@@ -22,7 +23,7 @@ const RootLayout = () => {
                 {/* <Stack.Screen name="about" options={{ title: 'About' }} />
                 <Stack.Screen name="contact" options={{ title: 'Contact', headerShown: false }} /> */}
             </Stack>
-        </>
+        </ UserProvider>
     )
 }
 
