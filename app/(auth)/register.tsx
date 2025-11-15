640e1bdccf22a454083cd8f8be5c2303fa748352
QUIROZ SOLDAN MARIO ALBERTO <marioaquirozs@gmail.com>
2025-11-14 19:51:52 -0600

    feat: implement registration form with error handling and user feedback

--- app/(auth)/register.tsx
+++ app/(auth)/register.tsx
@@ -1,7 +1,9 @@
 import { useState } from 'react';
+import { useUser } from '../../hooks/useUser';
 
 import { Keyboard, StyleSheet, Text, TouchableWithoutFeedback } from 'react-native';
 import { Link } from 'expo-router';
+import { Colors } from '../../constants/Colors';
 
 // themed components
 import ThemedView from '../../components/ThemedView';
@@ -14,28 +16,37 @@ const Register = () => {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [error, setError] = useState(null);
 
-    const handleSubmit = () => {
-        console.log("Register for submit: ", email, password);
+    const { register } = useUser();
+
+    const handleSubmit = async () => {
+        setError(null);
+
+        try {
+            await register(email, password);
+        } catch (error) {
+            setError(error.message);
+        }
     }
 
     return (
         <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
             <ThemedView style={styles.container}>
-                
+
                 <Spacer />
                 <ThemedText title={true} style={styles.title}>
                     Register for an account
                 </ThemedText>
 
-                <ThemedTextInput 
+                <ThemedTextInput
                     style={{ width: '80%', marginBottom: 20 }}
                     placeholder='Email'
                     keyboardType='email-address'
                     onChangeText={setEmail}
                     value={email}
                 />
-                <ThemedTextInput 
+                <ThemedTextInput
                     style={{ width: '80%', marginBottom: 20 }}
                     placeholder='Password'
                     onChangeText={setPassword}
@@ -44,10 +55,15 @@ const Register = () => {
                 />
 
                 <ThemedButton onPress={handleSubmit}>
-                    <Text style={{color: '#f2f2f2'}}>Register</Text>
+                    <Text style={{ color: '#f2f2f2' }}>Register</Text>
                 </ThemedButton>
 
-                <Spacer height={100}/>
+                <Spacer />
+                {
+                    error && <Text style={styles.error}> {error} </Text>
+                }
+
+                <Spacer height={100} />
 
                 <Link href='/login'>
                     <ThemedText style={{ textAlign: 'center' }}>
@@ -74,5 +90,25 @@ const styles = StyleSheet.create({
         fontSize: 18,
         fontWeight: "bold",
         marginBottom: 30
+    },
+
+    btn: {
+        backgroundColor: Colors.primary,
+        padding: 15,
+        borderRadius: 5
+    },
+
+    pressed: {
+        opacity: 0.8
+    },
+
+    error: {
+        color: Colors.warning,
+        padding: 10,
+        backgroundColor: '#f5c1c8',
+        borderColor: Colors.warning,
+        borderWidth: 1,
+        borderRadius: 6,
+        marginHorizontal: 10
     }
 })
